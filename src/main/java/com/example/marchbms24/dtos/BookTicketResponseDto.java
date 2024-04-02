dc47e9e57c755809b9d056e6b8cd93bd6b984b88
RahulSandhan <rahulv.sandhanshive@gmail.com>
2024-04-03 03:57:01 +0530

    Code BMS 3 - Book Ticket

---
+++ src/main/java/com/example/marchbms24/dtos/BookTicketResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.marchbms24.dtos;
+
+import com.example.marchbms24.models.Ticket;
+import lombok.Data;
+
+@Data
+public class BookTicketResponseDto {
+
+    private Response response;
+    private Ticket ticket;
+}
---
+++ src/main/java/com/example/marchbms24/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.marchbms24.repositories;
+
+import ch.qos.logback.classic.spi.EventArgUtil;
+import com.example.marchbms24.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer > {
+
+    Optional<User> findById(int userId);
+
+}
